c16513fb3bf6bedbadd06ec03ad4dcd2e1ca9d2d
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2022-06-29 16:34:13 +0600

    plugin main php file added

---
+++ dealer-portal.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin Name:       Project Portal
+ * Plugin URI:        https://github.com/Mohib04/dealer-portal.git
+ * Description:       By Using Dealer Portal You can add your company's Dealer.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mohibbulla Munshi
+ * Author URI:        https://in.linkedin.com/in/mohib5g
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       dp
+ * Domain Path:       /languages
+ */
+?>
